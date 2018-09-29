d216c60ffb6c273af442d09053cf11697cc8dd1d
kaushalkishorersmt <kaushalkishorersmt@gmail.com>
2018-09-29 19:34:12 +0530

    Add fix for repo list.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -3,7 +3,7 @@ class RepositoriesController < ApplicationController
   def index
     @repositories = []
     if current_user.present?
-      auth_result = JSON.parse(RestClient.get('https://api.github.com/user/repos', {:params => {:access_token => '075b67470ce2ff8f8ce26d4ae12e3ca57774e45a'} }))
+      auth_result = JSON.parse(RestClient.get('https://api.github.com/user/repos', {:params => {:access_token => current_user.oauth_token } }))
       auth_result.each do |x|
         @repositories << {name: x['name'], full_name: x['full_name']}
       end
