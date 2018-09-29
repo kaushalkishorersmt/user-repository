f1515e2890f36f962625cf0ed876023f13fe0c2b
kaushalkishorersmt <kaushalkishorersmt@gmail.com>
2018-09-29 12:21:12 +0530

    Add fix for login error.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
 opts = { scope: 'user:email' }
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, Rails.application.secrets.github_client_id, Rails.application.secrets.github_client_secret, opts
+  if Rails.env.production?
+    provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'], opts
+  else
+    provider :github, Rails.application.secrets.github_client_id, Rails.application.secrets.github_client_secret, opts
+  end
 end
