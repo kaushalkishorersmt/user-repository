51501495c50264c97e0bcfe26b266ba3a409585d
kaushalkishorersmt <kaushalkishorersmt@gmail.com>
2018-09-30 16:37:20 +0530

    Add commits to the show.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -10,11 +10,11 @@ class RepositoriesController < ApplicationController
     else
       redirect_to root_path, :notice => "Unauthorized."
     end
-
   end
 
   def show
     @repository = nil
+    @repository_commits = []
     if current_user.present?
       url = "https://api.github.com/repos/#{params[:format]}"
       auth_result = JSON.parse(RestClient.get(url, {:params => {:access_token => current_user.oauth_token } }))
@@ -23,6 +23,17 @@ class RepositoriesController < ApplicationController
     else
       redirect_to root_path, :notice => "Unauthorized."
     end
+
+    commits_url = "https://api.github.com/repos/#{params[:format]}/commits"
+    repo_commits = JSON.parse(RestClient.get(commits_url , {:params => {:access_token => current_user.oauth_token } }))
+    repo_commits.each do |x|
+      @repository_commits << {commit_msg: x['commit']['message'], commit_date: x['commit']['author']['date']}
+    end
+
   end
 
+  # "commits_url": "https://api.github.com/repos/benlcollins/github_api_viz/commits{/sha}",
+  # "git_commits_url": "https://api.github.com/repos/benlcollins/github_api_viz/git/commits{/sha}",
+  # GET /repos/:owner/:repo/commits
+  # https://api.github.com/repos/kaushalkishorersmt/github_api_viz
 end
